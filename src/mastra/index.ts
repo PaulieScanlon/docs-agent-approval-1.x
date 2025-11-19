702a73cd68138b3bdc2e1a92a6f625f62810374f
Paul Scanlon <pauliescanlon@gmail.com>
2025-11-19 16:39:33 +0000

    chore: add observability

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,5 +1,7 @@
 import { Mastra } from "@mastra/core/mastra";
 import { LibSQLStore } from "@mastra/libsql";
+import { PinoLogger } from "@mastra/loggers";
+import { Observability } from "@mastra/observability";
 
 import { planningAgent } from "./agents/planning-agent";
 import { galleryTool } from "./tools/activity-tool";
@@ -7,9 +9,14 @@ import { weatherTool } from "./tools/weather-tool";
 
 export const mastra = new Mastra({
   agents: { planningAgent },
-  tools: { weatherTool, galleryTool},
+  tools: { weatherTool, galleryTool },
+  logger: new PinoLogger(),
   storage: new LibSQLStore({
     id: "mastra-storage",
     url: ":memory:"
+  }),
+  observability: new Observability({
+    // Enables Tracing
+    default: { enabled: true }
   })
 });
